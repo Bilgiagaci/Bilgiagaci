178f8ff01cdda2df858d41b5936c37b1b89bbe45
Bilgiagaci <Bilgiagaci11@hotmail.com>
2025-07-17 23:43:50 +0300

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "short_name": "Bilgi Ağacı",
+  "name": "Bilgi Ağacı - 지식의 나무",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#79A6DC",
+  "theme_color": "#79A6DC",
+  "icons": [
+    {
+      "src": "/images/oyun-ikonu.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "/images/oyun-ikonu.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ]
+}
